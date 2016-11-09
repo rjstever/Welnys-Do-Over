0f5b6a5f187a45a5c2a2e522885cfb00ad2a534a
rjstever <rjstever@Roberts-MacBook-Pro.local>
2016-11-08 19:44:19 -0500

    first commit

---
+++ js/script.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+
+	$('#menulink').click(function(event) {
+		event.preventDefault();
+		if($('.navigation-wrapper').hasClass('show-menu')) {
+			$('.navigation-wrapper').removeClass('show-menu');
+			$('.navigation').hide();
+			$('.navigation li').removeClass('small-padding');
+		} else {
+			$('.navigation-wrapper').addClass('show-menu');
+			$('.navigation').fadeIn();
+			$('.navigation li').addClass('small-padding');
+	   }
+	});
+  
+});
+
