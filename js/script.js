e37be313aa302970df45c8c5afaf95ab4a9222ac
rjstever <rjstever@Roberts-MBP.fios-router.home>
2016-11-14 21:08:33 -0500

    4s

--- js/script.js
+++ js/script.js
@@ -31,26 +31,47 @@ Works but doesnt know form header
 
 $( document ).ready(function() {
 
-	// Show ant hide the form
-
 	$(".pop-form").click(function(event) {
-		// stop the href of the <a> from doing anything
 		event.preventDefault();
 
-		// get the title of the service where the button is clicked
 		var formTitle = $(this).prevAll("h3").text();
-		// write that title to the form title
 		$(".info-form .form-title").text(formTitle);
-		//show the form
 		$(".info-form").show();
 	});
 
 
 	$("#p-close").click(function(event) {
-		// stop the href of the <a> from doing anything
 		event.preventDefault();
-		//hide the form
 		$(".info-form").hide();
+	 });
+
+
+/*
+
+
+	Grrrrrrrrrrr
+
+*/
+
+	 $(".left").click(function(){
+		 event.preventDefault();
+		 var leftVal = $('.prod-wrap').css("marginLeft");
+
+		 if(leftVal !== "0px") {
+			 $('.prod-wrap').animate({
+				 marginLeft: "+=1200",
+			 }, 500);
+		 }
+	 });
+
+	 $(".right").click(function(){
+		 event.preventDefault();
+		 var leftVal = $('.prod-wrap').css("marginLeft");
+		 if(leftVal !== "-3600px") {
+			 $('.prod-wrap').animate({
+				 marginLeft: "-=1200",
+			 }, 500);
+		 }
 	 });
 
   });
